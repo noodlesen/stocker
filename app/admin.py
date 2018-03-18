d88e36a35f069af5fb88f58885e9f8415cf02f6c
noodlesen <k.lapshov@gmail.com>
2018-03-18 15:03:17 +0300

    ...

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project,Footage, Motion, MarketPlace
 
 # Register your models here.
+
+#class ProjectAdmin(admin.ModelAdmin):
+
+admin.site.register(Project)
+admin.site.register(Footage)
+admin.site.register(Motion)
+admin.site.register(MarketPlace)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class MarketPlace(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=50)
+
+class Motion(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=20)
+
+
+class Project(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=50)
+    folder = models.CharField(max_length=50)
+
+
+class Footage(models.Model):
+
+    def __str__(self):
+        return self.name + self.filename
+
+    name = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    motion = models.ForeignKey(Motion, on_delete='cascade')
+    project = models.ForeignKey(Project, on_delete = 'cascade')
+    finished = models.BooleanField(default=False)
+    rendered = models.BooleanField(default=False)
+    filename = models.CharField(max_length=50, blank=True, null=True)
+    resolution_x = models.IntegerField(default=4096)
+    resolution_y = models.IntegerField(default=2304)
+    has_alpha = models.BooleanField(default=False)
